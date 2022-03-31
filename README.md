39f80fefda06834fa98ecf585b8987c4951a8cfd
Dmytrocode <dmytrocontact@gmail.com>
2022-03-30 20:00:34 -0600

    finally solved dependency mistakes and re initialized the expo project, + installed nagivation libraries

--- README.md
+++
@@ -1,2 +0,0 @@
-# MessageYou
-Messaging Application built in React Native
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyANEhIAOK0VvGTe-kzMJ14xgQvmNVmozxE",
+    authDomain: "messageyou-254ef.firebaseapp.com",
+    projectId: "messageyou-254ef",
+    storageBucket: "messageyou-254ef.appspot.com",
+    messagingSenderId: "239998496883",
+    appId: "1:239998496883:web:dad11e8dae437ceb003a06",
+    measurementId: "G-0WGPWXKV1W"
+  };
